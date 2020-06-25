1f961268bceb842c72a1053c4283009f6eb15632
osman <osmnndm@protonmail.com>
2020-06-25 14:34:59 +0200

    zoomanlatımı ilk yükleme 1.version

---
+++ GitHubZoomAnlatımı.java
@@ -0,0 +1,24 @@
+package github;
+
+import java.util.Scanner;
+
+public class GitHubZoomAnlatımı {
+	
+	public static void main(String[] args) {
+		
+		// TOPLAMA
+		
+		Scanner scan = new Scanner (System.in);
+	    int num1 = scan.nextInt();
+	    int num2 = scan.nextInt();
+	    
+	   
+	    System.out.println(num1+num2);
+
+	    scan.close();
+
+		
+		
+	}
+
+}
