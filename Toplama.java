a14d540ca9e8b3b0f8d3f0b378c69ac9e30ed42a
osman <osmnndm@protonmail.com>
2020-06-25 14:40:30 +0200

    çıkarma methodunu ekledik

--- Toplama.java
+++ Toplama.java
@@ -1,5 +1,34 @@
 package github;
 
+import java.util.Scanner;
+
 public class Toplama {
+	
+	public static void main(String[] args) {
+		
+		// TOPLAMA
+		
+				Scanner scan = new Scanner (System.in);
+			    int num1 = scan.nextInt();
+			    int num2 = scan.nextInt();
+			    
+			   
+			    System.out.println(num1+num2);
+
+			    scan.close();
+			    
+			 // CIKARMA
+				
+				Scanner scan = new Scanner (System.in);
+			    int num3 = scan.nextInt();
+			    int num4 = scan.nextInt();
+			    
+			   
+			    System.out.println(num3-num4);
+
+			    scan.close();
+		
+		
+	}
 
 }
